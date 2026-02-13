84db0d63ac6033f6143b9fab39cda0b1a44f95b5
lipi301 <132067755+lipi301@users.noreply.github.com>
2026-02-13 09:40:08 +0000

    DATATYPES

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,35 @@
+"use strict";   //treat all js code as newer version
+
+//alert(3+3)    //we are using nodejs, not browser
+
+
+
+console.log(3
+    +
+    3) //code readability should be high
+
+
+ console.log("Lish")
+
+
+ let name ="Lish"                    //datatype : string
+ let age =18                       //datatype : number
+ let isLoggedIn=false                //datatype : boolean
+ let state;
+
+
+ //number => 2 to power 53
+ //bigint
+ //string => ""  ''
+ //boolean => true/false
+ //null => standalone value
+ //undefined => variable is only declared not assigned any value
+ //symbol=> unique
+
+ //object
+
+ console.log(typeof "Lish");
+ console.log(typeof age);
+ 
+ console.log(typeof undefined);   //undefined
+ console.log(typeof  null);        //OBJECT
