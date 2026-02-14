1f4bd441cec4c737a56de3e6b22ae6fab834aec9
lipi301 <132067755+lipi301@users.noreply.github.com>
2026-02-14 18:38:22 +0000

    dataTypeConversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,6 @@
+          /*convert to number*/
+
+
 //  let score= 33
 
 //  console.log(typeof score)
@@ -19,27 +22,85 @@
 //  let valueInNumber2= Number(score2)
 //  console.log(typeof valueInNumber2) 
 
-//  console.log(valueInNumber2)                      // NaN : Not a Number   
+//  console.log(valueInNumber2)                      // NaN : Not a Number   (datatype: number)
 
 
- let score3=null
- console.log(typeof score3)
+//  let score3=null
+//  console.log(typeof score3)
 
- let valueInNumber3= Number(score3)
- console.log(typeof valueInNumber3) 
+//  let valueInNumber3= Number(score3)
+//  console.log(typeof valueInNumber3) 
 
- console.log(valueInNumber3)                   //carefull: iska output hme 0 dedega.which might be a value in ur data eg temperature but null means empty
-
- 
- let score4=undefined
- console.log(typeof score4)
-
- let valueInNumber4= Number(score4)
- console.log(typeof valueInNumber4) 
-
- console.log(valueInNumber4)                   //output: NaN
-
+//  console.log(valueInNumber3)                   //carefull: iska output hme 0 dedega.which might be a value in ur data eg temperature but null means empty
 
  
+//  let score4=undefined
+//  console.log(typeof score4)
+
+//  let valueInNumber4= Number(score4)
+//  console.log(typeof valueInNumber4) 
+
+//  console.log(valueInNumber4)                   //output: NaN 
+
 
  
+//  let score5=true
+//  console.log(typeof score5)
+
+//  let valueInNumber5= Number(score5)
+//  console.log(typeof valueInNumber5) 
+
+//  console.log(valueInNumber5)    
+
+ 
+ 
+//  let score6="Lish"
+//  console.log(typeof score6)
+
+//  let valueInNumber6= Number(score6)
+//  console.log(typeof valueInNumber6) 
+
+//  console.log(valueInNumber6) 
+
+ 
+                             /*conversion to Number
+                             "33" => 33
+                             "33abc" => NaN
+                              true => 1; false =>0
+                              */
+            
+
+
+
+          /*convert to boolean*/
+
+// let isLoggedIn=1
+
+// let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+// let isLoggedIn_ =""
+// let booleanIsLoggedIn_= Boolean(isLoggedIn_)
+// console.log(booleanIsLoggedIn_)
+  
+
+// let isLoggedIn ="Lish"
+//let booleanIsLoggedIn= Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+                      /*  conversion to Boolean 
+                          1=> true , 0 => false
+                          "=" => false
+                          "Lish" => true
+                          */
+              
+
+          /*convert to string*/
+
+
+let isNumber =33
+let stringNumber = String(isNumber)
+ console.log(stringNumber)
+ console.log(typeof stringNumber)
+
