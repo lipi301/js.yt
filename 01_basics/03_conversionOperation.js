50834c7e8084cdf8895dc53be1462d65d6c22c4f
lipi301 <132067755+lipi301@users.noreply.github.com>
2026-02-14 15:05:18 +0000

    progress

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,42 @@
+//  let score= 33
+
+//  console.log(typeof score)
+//  console.log(typeof (score))
+
+
+//  let score1 ="33"
+//  console.log(typeof score1);                     //output:number(lower case mein likh hoga)
+
+//  let valueInNumber= Number(score1)               //yhan number k 'N' capital h: Number
+ 
+//  console.log(typeof valueInNumber) 
+//  console.log(valueInNumber) 
+
+
+//  let score2="33abc"
+//  console.log(typeof score2)
+
+//  let valueInNumber2= Number(score2)
+//  console.log(typeof valueInNumber2) 
+
+//  console.log(valueInNumber2)                      // NaN : Not a Number   
+
+
+ let score3=null
+ console.log(typeof score3)
+
+ let valueInNumber3= Number(score3)
+ console.log(typeof valueInNumber3) 
+
+ console.log(valueInNumber3)                   //carefull: iska output hme 0 dedega.which might be a value in ur data eg temperature but null means empty
+
+ 
+ let score4=undefined
+ console.log(typeof score4)
+
+ let valueInNumber4= Number(score4)
+ console.log(typeof valueInNumber4) 
+
+ console.log(valueInNumber4)                   //output: NaN
+
+ 
