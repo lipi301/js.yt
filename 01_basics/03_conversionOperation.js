d24417017ca7db1c155c17f79362bfb02c394dda
lipi301 <132067755+lipi301@users.noreply.github.com>
2026-02-16 11:27:08 +0000

    progress

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -117,6 +117,38 @@ let negValue= -value
 // console.log(2/3)
 // console.log(2%3)
 
+let str1= "Hello"
+let str2= " Lish"
+let str3= str1 + str2
+// console.log(str3)
+
+
+//  console.log("1" + 2)
+//  console.log(1 + "2")
+//  console.log("1" + 2 + 2)                  //agr string first h toh sab string ki trh treat honge 
+//  console.log(1 + 2 + "2")                  //string last mein h toh phle jo conversion/operation h ho jaega then treat them like string
+
+//  console.log(2 + 3 * 4 % 5)                //very bad ,messy way of code
+//  console.log((2 + 3) * 4 % 5)              //use roper parenthesis.more readability 
+
+
+// console.log(+true);                         //bad way
+// console.log(+"") 
+
+let num1 ,num2 , num3
+num1= num2 = num3= 2 + 2                   //low readability
+  
+let gameCounter =100
+gameCounter++                              //postfix increment
+console.log(gameCounter)
+
+++gameCounter
+console.log(gameCounter)
+
+
+
+
+
 
 
 
